6b36685d42fa47170e0e1abd31f4df89a8a40dc8
ItsmeCurly <adamg765@gmail.com>
2019-10-25 05:06:21 -0400

    adding listeners to server ref and userprofiles

--- bot/cogs/data/serverref.py
+++ bot/cogs/data/serverref.py
@@ -8,12 +8,19 @@ from discord.ext.commands import Bot, command, Cog
 class ServerRef(Cog):
     def __init__(self, bot):
         self.bot = bot
-        
+
+    @Cog.listener
+    def on_guild_join(self, guild: discord.Guild):
+        pass
+
 async def connect(message: discord.Message):
     conn = sqlite3.connect(database_path)
     c = conn.cursor()
-    
-    c.execute(sql="INSERT INTO serverref (id, guild_id, guild_name, channel, channel_name, last_channel_update, last_channel_activity, last_guild_update, last_guild_activity) VALUES (?,?,?,?,?,?,?,?,?)")
+
+    c.execute(sql="INSERT INTO serverref (guild_id, guild_name, channel, channel_name, last_channel_update, last_channel_activity, last_guild_update, last_guild_activity) VALUES (?,?,?,?,?,?,?,?,?)",
+    )
+
+    conn.close()
 
 def setup(bot):
     bot.add_cog(ServerRef(bot))
--- bot/cogs/data/userprofiles.py
+++ bot/cogs/data/userprofiles.py
@@ -2,6 +2,7 @@ import discord
 import sqlite3
 
 from bot.constants import database_path
+from datetime import datetime
 
 from discord.ext.commands import Bot, command, Cog, Context
 
@@ -14,17 +15,28 @@ class UserProfiles(Cog):
     async def on_user_update(self, before: discord.User, after: discord.User) -> None:
         pass
 
-def connect(update: discord.User) -> None:
+    @Cog.listener()
+    async def on_member_update(self, before: discord.Member, after: discord.Member) -> None:
+        member_connect(after)
+
+def user_connect(user: discord.User) -> None:
     conn = sqlite3.connect(database = database_path)
     c = conn.cursor()
 
-    c.execute('INSERT INTO userprofiles (id, userid, name, guild_name, guild_display_name, avatar_url, created_at, last_updated, last_online) VALUES (?,?,?,?,?,?,?,?,?)')
+    c.execute('INSERT INTO userprofiles (userid, name, guild_name, guild_display_name, avatar_url, created_at, last_updated, last_online) VALUES (?,?,?,?,?,?,?,?,?)',
+              (user.id, user.name, "", user.display_name, user.avatar_url, user.created_at, ))
+
+    conn.close()
+
+def member_connect(member: discord.Member) -> None:
+    conn = sqlite3.connect(database=database_path)
+    c = conn.cursor()
+
+    c.execute('INSERT INTO userprofiles (userid, name, guild_name, guild_display_name, avatar_url, created_at, last_updated, last_online) VALUES (?,?,?,?,?,?,?,?,?)',
+    ())
 
     conn.close()
 
 def setup(bot):
     bot.add_cog(UserProfiles(bot))
     print("Loaded cog: Data.UserProfiles")
-
-
-
