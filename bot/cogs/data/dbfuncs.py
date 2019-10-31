61e0319910a06fd7918d2ab1aaf840abc3be7168
ItsmeCurly <adamg765@gmail.com>
2019-10-31 03:20:53 -0400

    added create_table and drop table bot commands

--- bot/cogs/data/dbfuncs.py
+++ bot/cogs/data/dbfuncs.py
@@ -7,18 +7,32 @@ from discord.ext.commands import Bot, Cog, Context, command
 from bot.cogs.data.dbref import recent_message_connect
 from bot.constants import database_path
 from bot.decorators import developer
-from bot.utils.database import print_table_structure
+from bot.utils.database import create_messages_table, create_serverref_table, create_userprofiles_table
 
 
 class DbFuncs(Cog):
     def __init__(self, bot):
         self.bot = bot
-        
+
     @command()
     @developer
     def delete_table(self, ctx: Context, table_name:str) -> None:
-        pass
-        
+        conn = sqlite3.connect(database_path)
+        c = conn.cursor()
+
+        c.execute(f"""DROP TABLE {table_name}""")
+
+        conn.commit()
+        conn.close()
+    @command()
+    @developer
+    def create_table(self, ctx: Context, table_name: str) -> None:
+        {
+            'messages': create_messages_table,
+            'serverref': create_serverref_table,
+            'userprofiles': create_userprofiles_table
+        }.get(table_name.lower(), lambda: None)()
+
 def setup(bot) -> None:
     bot.add_cog(DbFuncs(bot))
     print("Loaded cog: Data.DbFuncs")
