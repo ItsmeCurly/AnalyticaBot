f96d2071c3732b8ad28e118bcd316b86095ab164
Adam <adamg765@gmail.com>
2019-11-05 12:45:39 -0500

    code refactoring

--- bot/utils/database.py
+++ bot/utils/database.py
@@ -5,74 +5,85 @@ import sqlite3
 #from bot.constants import database_path, config_path
 
 def quick_execute_sql_command(*, command, commit = False):
-    conn, c = get_cursor()
+    with get_cursor() as (conn, c):
+        c.execute(command)
+        
+        conn.commit() if commit else None
 
-    c.execute(command)
-
-    (conn.commit(), None)[commit]
-
-    conn.close()
+        conn.close()
     
 def create_messages_table():
     """creates the messages table"""
 
     quick_execute_sql_command(
-        command='''CREATE TABLE messages
-        (id integer primary key,
-        message_id integer,
-        member_id integer,
-        content text,
-        channel_id integer,
-        guild_id integer,
-        embeds text,
-        attachments text,
-        reactions text,
-        time timestamp,
-        edited integer,
-        last_edited_at timestamp,
-        message_type text)''', commit = True)
+        command="""
+        CREATE TABLE 
+            messages
+        (
+            id integer primary key,
+            message_id integer,
+            member_id integer,
+            content text,
+            channel_id integer,
+            guild_id integer,
+            embeds text,
+            attachments text,
+            reactions text,
+            time timestamp,
+            edited integer,
+            last_edited_at timestamp,
+            message_type text
+        )""", commit = True)
 
 
 def create_userprofiles_table():
     """creates the user profiles table"""
 
     quick_execute_sql_command(
-        command='''CREATE TABLE userprofiles
-        (id integer primary key,
-        userid integer,
-        discriminator integer,
-        name text,
-        guild_id integer,
-        guild_user_display_name text,
-        avatar_url text,
-        premium_since datetime,
-        status text,
-        mobile_status text,
-        desktop_status text,
-        web_status text,
-        roles text,
-        activities text,
-        created_at datetime,
-        joined_at datetime,
-        last_updated timestamp,
-        last_online timestamp,
-        update_type text,
-        possible_change text)''', commit=True)
+        command="""
+        CREATE TABLE 
+            userprofiles
+        (
+            id integer primary key,
+            userid integer,
+            discriminator integer,
+            name text,
+            guild_id integer,
+            guild_user_display_name text,
+            avatar_url text,
+            premium_since datetime,
+            status text,
+            mobile_status text,
+            desktop_status text,
+            web_status text,
+            roles text,
+            activities text,
+            created_at datetime,
+            joined_at datetime,
+            last_updated timestamp,
+            last_online timestamp,
+            update_type text,
+            possible_change text
+        )""", commit=True)
 
 def create_serverref_table():
     """creates server reference table"""
 
     quick_execute_sql_command(
-        command='''CREATE TABLE serverref
-        (id integer primary key,
-        guild_id integer,
-        guildname text,
-        channel integer,
-        channelname text,
-        last_channel_update timestamp,
-        last_channel_activity timestamp,
-        last_guild_update timestamp,
-        last_guild_activity timestamp)''', commit=True)
+        command="""
+        CREATE TABLE 
+            serverref
+        (
+            id integer primary key,
+            guild_id integer,
+            guildname text,
+            channel integer,
+            channelname text,
+            last_channel_update timestamp,
+            last_channel_activity timestamp,
+            last_guild_update timestamp,
+            last_guild_activity timestamp
+        )""", commit=True)
 
 def del_connection(table_name: str) -> None:
     confirm = input(f"This will delete table {table_name}, confirm? (Y/N): ")
@@ -85,67 +96,62 @@ def del_connection(table_name: str) -> None:
         return
 
 def check_exists_table(table_name: str) -> None:
-    conn, c = get_cursor()
+    with get_cursor() as (conn, c):
+        c.execute(f"""
+                SELECT
+                    name 
+                FROM 
+                    sqlite_master 
+                WHERE 
+                    type='table' AND name='{table_name}'
+                """)
 
-    c.execute(f"""SELECT name FROM sqlite_master WHERE type='table' AND name=
-              '{table_name}'""")
-
-    group = c.fetchone()
-
-    conn.close()
-
-    return (group != None and table_name == group[0])
+        group = c.fetchone()
+        return (group != None and table_name == group[0])
 
 def print_table_structure(table_name: str) -> None:
-    conn, c = get_cursor()
+    with get_cursor() as (conn, c):
+        c.execute(f"pragma table_info('{table_name}')")
 
-    c.execute(f"pragma table_info('{table_name}')")
-
-    print(c.fetchall())
-
-    conn.close()
+        print(c.fetchall())
 
 def pprint_table_structure(table_name: str) -> str:
-    conn, c = get_cursor()
-    c.execute(f"pragma table_info('{table_name}')")
+    with get_cursor() as (conn, c):
+        c.execute(f"pragma table_info('{table_name}')")
 
-    _list_structure = c.fetchall()
+        _list_structure = c.fetchall()
 
-    _return_str = str(_list_structure[0][1])
-    for ele in _list_structure[1:]:
-        _return_str = _return_str + " " * 16 + ele[1]
-
-    conn.close()
-    return _return_str
+        _return_str = str(_list_structure[0][1])
+        for ele in _list_structure[1:]:
+            _return_str = _return_str + " " * 16 + ele[1]
+        
+        return _return_str
 
 def pprint_table_preview(table_name: str) -> list:
     table_structure = pprint_table_structure(table_name)
 
     table_structure_list = table_structure.split(" ")
 
-    conn, c = get_cursor()
-    _str = ""
-    for row_message in c.execute(f"""SELECT * FROM {table_name} ORDER BY id ASC 
-                                 LIMIT """ + str(5)):
-        spacing = table_structure_list[0]
-        _str = f"%{spacing}"
-
-    conn.close()
+    with get_cursor() as (conn, c):
+        _str = ""
+        for row_message in c.execute(f"""SELECT * FROM {table_name} ORDER BY id ASC 
+                                    LIMIT """ + str(5)):
+            spacing = table_structure_list[0]
+            _str = f"%{spacing}"
 
 
 def get_table_structure(table_name: str) -> str:
-    conn, c = get_cursor()
+    with get_cursor() as (conn, c):
 
-    c.execute(f"pragma table_info('{table_name}')")
+        c.execute(f"pragma table_info('{table_name}')")
 
-    _list = c.fetchall()
-    conn.close()
-    _str = ""
+        _list = c.fetchall()
+        _str = ""
 
-    for ele in _list:
-        _str = "|".join([_str, " ".join(str(i) for i in ele)])
+        for ele in _list:
+            _str = "|".join([_str, " ".join(str(i) for i in ele)])
 
-    return _str
+        return _str
 
 def check_table_structure(table_name: str) -> bool:
     config = configparser.ConfigParser()
@@ -164,14 +170,12 @@ def alter_table(table: str):
     pass
 
 def print_tables() -> list:
-    conn, c = get_cursor()
+    with get_cursor() as (conn, c):
+        c.execute("SELECT name FROM sqlite_master WHERE type='table'")
 
-    c.execute("SELECT name FROM sqlite_master WHERE type='table'")
-
-    _to_return = c.fetchall()
-
-    conn.close()
-    return _to_return
+        _to_return = c.fetchall()
+        
+        return _to_return
 
 def get_cursor() -> (sqlite3.Connection, sqlite3.Cursor):
     conn = sqlite3.connect(database = 'db\\analyticadatapoints.db') #database_path
--- bot/utils/startup.py
+++ bot/utils/startup.py
@@ -9,27 +9,6 @@ from bot.constants import CONFIG_PATH, DATABASE_PATH, PREFIXES_PATH
 tables = ["messages", "serverref", "userprofiles"]
 table_creation = [db.create_messages_table, db.create_serverref_table, db.create_userprofiles_table]
 
-def main() -> None:
-    #create config file
-
-    if not path.exists(CONFIG_PATH):
-        create_config(CONFIG_PATH)
-
-    #create dbs
-
-    if not path.exists(DATABASE_PATH):
-        pass
-        #create db somehow
-
-    """ for i in range(len(tables)):
-        if not db.check_exists_table(table_name = tables[i]) or not db.check_table_structure(table_name = tables[i]):
-            table_creation[i]() """
-
-    #create prefixes json
-    if not path.exists(PREFIXES_PATH):
-        pass
-        #create json file
-
 def read_token() -> str:
     cfg = configparser.ConfigParser()
     cfg.read('config.ini')
@@ -38,8 +17,5 @@ def read_token() -> str:
 
     if token == None:
         raise ValueError("No token supplied in config, please supply a token")
-
+    
     return cfg['Token']['token']
-
-if __name__ == "__main__":
-    main()
