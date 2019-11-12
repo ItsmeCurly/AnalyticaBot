7335d836067fc16030f1c5973f87781e8a56a3e6
ItsmeCurly <adamg765@gmail.com>
2019-11-12 04:45:14 -0500

    adding to music cog

--- bot/cogs/music.py
+++ bot/cogs/music.py
@@ -9,12 +9,27 @@ OPUS_LIBS = ['libopus-0.x86.dll', 'libopus-0.x64.dll', 'libopus-0.dll', 'libopus
 class Music(Cog):
     def __init__(self, bot):
         self.bot = bot
-        load_opus_lib()
+        #self.voice_client = None
+        #load_opus_lib()
+
+    @command()
+    async def join(self, ctx: Context):
+        print("a")
+        # if not (chan := ctx.author.voice.channel):
+        #     return
+        # self.voice_client = await chan.connect()
+
+    @command()
+    async def leave(self, ctx:Context):
+        pass
+        # if not self.voice_client:
+        #     return
+        # await self.voice_client.disconnect()
 
     @command()
     async def play(self, ctx: Context, sound_name: str):
         pass
-      
+
 def load_opus_lib():
     if opus.is_loaded():
         return True
@@ -25,10 +40,10 @@ def load_opus_lib():
             return
         except OSError:
             pass
-        raise RuntimeError('Could not load an opus lib. Tried %s' % (', '.join(OPUS_LIBS)))  
-        
+        raise RuntimeError('Could not load an opus lib. Tried %s' % (', '.join(OPUS_LIBS)))
+
 def create_audio_source(file_name: str) -> discord.AudioSource:
-    discord.FFmpegPCMAudio()
+    discord.FFmpegPCMAudio(source="music\\sound.mp3")
 
 def setup(bot):
     bot.add_cog(Music(bot))
